e3d8a9a3bab3dc7e1d2b4ee0e7563acb53be6c6e
Emil <emil-ep@hotmail.com>
2020-03-11 13:29:06 +0100

    Initial commit

---
+++ Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsUCL.Models
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
